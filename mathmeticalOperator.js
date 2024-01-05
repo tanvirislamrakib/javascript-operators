69185db16b399949fc0125746ae0832764ffd8ee
tanvirislamrakib <155340897+tanvirislamrakib@users.noreply.github.com>
2024-01-05 18:04:30 +0600

    Add files via upload

---
+++ mathmeticalOperator.js
@@ -0,0 +1,21 @@
+var num1 = 50;
+var num2 = 50;
+//var total = num1 + num2;
+console.log(num1 + num2);
+
+var num1 = 60;
+var num2 = 70;
+console.log(num1 - num2);
+console.log(num2 - num1);
+
+var num1 = 20;
+var num2 = 2;
+console.log(num1 * num2);
+
+var num1 = 20;
+var num2 = 2;
+console.log(num1/num2);
+
+var num1 = 30;
+var num2 = 4;
+console.log(num1%num2);
